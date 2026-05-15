85d13a3531dd0db0b312a5237f7833b285597155
Samxlone <mirzasajidali31@gmail.com>
2026-05-15 22:47:19 +0530

    Built password checker using while loop

---
+++ password_checker.py
@@ -0,0 +1,9 @@
+password = ""
+
+while password != "python123":
+    password = input("Enter password: ")
+
+    if password != "python123":
+        print("Wrong password 😭 Try again")
+
+print("Access granted 😈")
